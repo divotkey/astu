eacc79a365abad6ae65b0719dbbefb58b1388327
Roman Divotkey <roman.divotkey@gmail.com>
2021-06-24 10:47:29 +0200

    Removed obsolete code.

--- src/srv/Service.cpp
+++ src/srv/Service.cpp
@@ -10,7 +10,6 @@
 
 // Local includes
 #include "ServiceManager.h"
-#include "SignalService.h"
 #include "Events.h"
 #include "Service.h"
 
@@ -82,87 +81,5 @@ namespace astu {
 
         status = Stopped;
     }
-
-
-    /////////////////////////////////////////////////
-    /////// Other things
-    /////////////////////////////////////////////////
-
-    // const std::string & BaseService::DEFAULT_NAME = "UNNAMED";
-
-    // BaseService::BaseService(const std::string & aName)
-    //     : name(aName)
-    //     , running(false)
-    // {
-    //     // Intentionally left empty
-    // }
-
-    // const std::string & BaseService::GetName() const
-    // {
-    //     return name;
-    // }
-
-    // void BaseService::Startup()
-    // {
-    //     if (IsRunning()) {
-    //         throw std::logic_error("Service " + GetName() + " already running");
-    //     }
-
-    //     // START OF EXPERIMENTAL CODE
-    //     for (auto & hook : startupHooks) {
-    //         hook();
-    //     }
-    //     // END OF EXPERIMENTAL CODE
-
-    //     OnStartup();
-    //     running = true;
-    //     auto srvEvents = ASTU_GET_SERVICE_OR_NULL(SignalService<ServiceEvent>);
-    //     if (srvEvents) {
-    //         srvEvents->FireSignal(ServiceEvent(ServiceEvent::Started, *this));
-    //     }
-    // }
-
-    // void BaseService::Shutdown()
-    // {
-    //     // Best practice: ignore shut down calls on not-running services.
-    //     if (IsRunning()) {
-    //         OnShutdown();
-
-    //         // START OF EXPERIMENTAL CODE
-    //         for (auto & hook : shutdownHooks) {
-    //             hook();
-    //         }
-    //         // END OF EXPERIMENTAL CODE
-
-    //         running = false;
-
-
-    //         auto srvEvents = ASTU_GET_SERVICE_OR_NULL(SignalService<ServiceEvent>);
-    //         if (srvEvents) {
-    //             srvEvents->FireSignal(ServiceEvent(ServiceEvent::Stopped, *this));
-    //         }
-    //     }
-    // }
-
-    // bool BaseService::IsRunning() const
-    // {
-    //     return running;
-    // }
-
-    // ServiceManager & BaseService::GetSM()
-    // {
-    //     return ServiceManager::GetInstance();
-    // }
-
-    // void BaseService::AddStartupHook(std::function<void (void)> func)
-    // {
-    //     startupHooks.push_back(func);
-    // }
-
-    // void BaseService::AddShutdownHook(std::function<void (void)> func)
-    // {
-    //     shutdownHooks.push_back(func);
-    // }
-
 
 } // end of namespace
