172f195c3c2b44cc92bdf5382a0d176bae3bafff
Roman Divotkey <roman.divotkey@gmail.com>
2021-10-11 07:36:10 +0200

    Extended qtable class.

--- include/Ai/QTable.h
+++ include/Ai/QTable.h
@@ -91,8 +91,21 @@ namespace astu {
 		 * @param action	the action to update
 		 * @param reward	the reward for the specified state and action
 		 * @param nextState	the next state to following the specified state and action
+		 * @return the selected best future action of teh next state
 		 */
 		size_t Update(size_t state, size_t action, double reward, size_t nextState);
+
+		/**
+		 * Updates this Q-Table (without next state).
+		 * 
+		 * Use this method for updating if there is no next state e.g., if the
+		 * end of an episode has been reached.
+		 * 
+		 * @param state		the state to update
+		 * @param action	the action to update
+		 * @param reward	the reward for the specified state and action
+		 */
+		void Update(size_t state, size_t action, double reward);
 
 		/**
 		 * Returns the index of the action with the actual maximum value.
@@ -106,7 +119,8 @@ namespace astu {
 		 * Sets the learning rate.
 		 *
 		 * @param alpha	the learning rate
-		 * @throws std::invalid_argument in case the learning rate is less than zero or greater than one
+		 * @throws std::invalid_argument in case the learning rate is less than
+		 * 		zero or greater than one
 		 */
 		void SetLearningRate(double alpha);
 
@@ -150,8 +164,102 @@ namespace astu {
 		 */
 		void RestoreFromMento(const  Memento & m);
 
+		/**
+		 * Returns the number of updates a particular state has received so far.
+		 * 
+		 * @param state	the state index
+		 * @return the number of updates
+		 */
+		size_t GetStateUpdates(size_t state) const;
+
+		/**
+		 * Returns the average value for all actions of a state.
+		 * 
+		 * @param state	the state index
+		 * @return the average value of all actions
+		 */
+		double GetAverageValue(size_t state) const;
+
+		/**
+		 * Returns the standard deviation for the values of all actions.
+		 * 
+		 * @param state	the state index
+		 * @return the standard deviation of all actions
+		 */
+		double GetStandardDeviationValue(size_t state) const;
+
+		/**
+		 * Return the maximum standard deviation value of all states.
+		 * 
+		 * @param the maximum standard deviation value.
+		 */
+		double GetMaxStandardDeviationValue() const;
+
+		/**
+		 * Returns the maximum number of updates a state has received.
+		 * 
+		 * @param idx	the index in the sorted update count list
+		 * @return the maximum number of updates
+		 */
+		size_t GetMaxUpdates(size_t idx = 0) const;
+
+		/**
+		 * Returns the minimum number of updates a state has received.
+		 * 
+		 * @return the minimum number of updates
+		 */
+		size_t GetMinUpdates() const;
+
+		/**
+		 * Returns the average number of updates states have received.
+		 * 
+		 * @return the average number of updates
+		 */
+		double GetAvgUpdates() const;
+
+		/**
+		 * Returns the median number of updates states have received.
+		 * 
+		 * @return the median number of updates
+		 */
+		double GetMedianUpdates() const;
+
+		/**
+		 * Number of states which have less or equal the specified amount of updates.
+		 * 
+		 * @param maxUpdates	the maximum number of updates
+		 * @return the number of states that satisfy the condition
+		 */
+		size_t CountStatesWithLimitedUpdates(size_t maxUpdates = 0) const;
+
+		struct StateEntry {
+			size_t state;
+			size_t numUpdates;
+
+			StateEntry(size_t state, size_t numUpdates)
+				: state(state), numUpdates(numUpdates) {}
+
+			bool operator >(const StateEntry& rhs) {
+				return numUpdates > rhs.numUpdates;
+			}
+		};
+
+		/**
+		 * Returns a list of states sorted by the number of their updates.
+		 * 
+		 * The states are sorted in descending order, which means that the
+		 * first state in the list has received the most updates so far.		 
+		 */
+		const std::vector<StateEntry>& GetUpdateDistribution() const;
+
+
 		// DBG
-		void UpdateStats();
+		size_t GetVisits(size_t entryIdx) const;
+
+		/**
+		 * Updates the statistical data.
+		 */
+		void UpdateStats() const;
 
 		unsigned int GetMaxVisits() const {
 			return maxVisits;
@@ -189,6 +297,7 @@ namespace astu {
 			void RestoreFromMemento(const  Memento & m);
 		};
 
+
 		/** The entries of this table. */
 		std::unique_ptr<Entry[]> m_entries;
 
@@ -199,13 +308,25 @@ namespace astu {
 		const size_t numActions;
 
 		/** The maximum visits of entries. */
-		unsigned int maxVisits;
+		mutable unsigned int maxVisits;
 
 		/** The minimum visits of entries. */
-		unsigned int minVisits;
+		mutable unsigned int minVisits;
 
 		/** The average visits of entries. */
-		double avgVisits;
+		mutable double avgVisits;
+
+		/** The maximum number of updates a state has received so far. */
+		mutable size_t maxUpdates;
+
+		/** The maximum number of updates a state has received so far. */
+		mutable size_t minUpdates;
+
+		/** Sorted list of states by number of updates. */
+		mutable std::vector<StateEntry> updateList;
+
+		/** Indicates that statistical data require updates. */
+		mutable bool statsDirty;
 
 		/** The number of updates this table has revived. */
 		size_t numUpdates;
@@ -215,6 +336,7 @@ namespace astu {
 
 		/** The discount factor. */
 		double gamma;
+
 
 		/**
 		 * Calculates the index of the entry.
