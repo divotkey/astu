163b81779f95a52670d382cb2606e7fdf9bf7551
Roman Divotkey <roman.divotkey@gmail.com>
2022-07-29 11:16:30 +0200

    Work continued.

---
+++ src/XOS/Linux/XosSleep.cpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2020-2022 Roman Divotkey. All rights reserved.
+//
+// This file is subject to the terms and conditions defined in file 'LICENSE',
+// which is part of this source code package. See 'AUTHORS' file for a list
+// of contributors.
+
+// Local includes
+#include "XosSleep.h"
+
+namespace astu {
+
+    XosSleep::XosSleep() {
+        // Intentionally left empty.
+    }
+
+    XosSleep::~XosSleep() {
+        // Intentionally left empty.
+    }
+
+    void XosSleep::Sleep(std::chrono::nanoseconds ns) {
+        // Intentionally left empty.
+    }
+
+} // end of namespace
